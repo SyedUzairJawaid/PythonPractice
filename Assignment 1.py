67d83b19b66cb23e56d2ce67b7ff41e1fb678d91
SyedUzairJawaid <57342326+SyedUzairJawaid@users.noreply.github.com>
2019-11-04 22:52:59 +0500

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+a = '''Twinkle, twinkle, little star, 
+              How I wonder what you are! 
+                    Up above the world so high, 
+                    Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+How I wonder what you are'''
+print(a)
+
+
+# In[25]:
+
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+
+# In[ ]:
+
+
+first_name = "Syed Uzair"
+last_name = "Jawaid"
+name = last_name + " " + first_name
+print(name)
+
+
+# In[ ]:
+
+
+asian_country = "Pakistan"
+european_country = "Spain"
+countries = asian_country + " " + euorpean_country
+print(countries)
+
